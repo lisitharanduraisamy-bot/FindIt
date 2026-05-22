e90058f35cd5a1760d335815bf499aa2c0de6862
Revanth02 <revanthp0201@gmail.com>
2026-05-22 14:37:36 +0530

    fix: unignore config.js to enable live db on netlify

---
+++ js/config.js
@@ -0,0 +1,4 @@
+export const CONFIG = {
+    supabaseUrl: "https://nkstmejwkbgeoguytpyq.supabase.co",
+    supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5rc3RtZWp3a2JnZW9ndXl0cHlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkzNTgyNzUsImV4cCI6MjA5NDkzNDI3NX0.oZB24Gz2Amj4XjX19tjuj6AP-FClNFTiOAJPXwpQc7Q"
+};
